2a6f30f30cfdbb4c222609cf8b4ddcc04950786d
ChrisCodes <68878422+ChristianMods@users.noreply.github.com>
2021-02-28 11:38:02 -0500

    final push

---
+++ README.md
@@ -0,0 +1,12 @@
+### Setup
+
+go to https://discord.com/developers/applications and make a app
+
+
+### Install
+
+Clone/Download the file
+
+Open a cmd in the folder and type npm install -> (this installs all dependencies)
+
+Open the index and change it to your liking (make sure you put your client id) (i will leave comments explaning each step)
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-var rpc = require("discord-rpc")
-const client = new rpc.Client({ transport: 'ipc' })
-let clientName = 'FNBRChris-RPC'
+var rpc = require("discord-rpc") // require the discord-rpc package
+const client = new rpc.Client({ transport: 'ipc' }) // new rpc client
+let clientName = 'FNBRChris-RPC' // your name which is a var which is used in the log when it goes online
 client.on('ready', () => {
 client.request('SET_ACTIVITY', {
 pid: process.pid,
-activity : {
-details : "Code FNBRChris",
+activity : { // btw thats my fn code if you want to use it :)
+details : "Code FNBRChris", // text -> any text... youll see where it goes when you start it
 assets : {
-large_image : "code_fnbrchris",
-large_text : "USING CODE FNBRChris"
+large_image : "code_fnbrchris", // copy the image name from https://discord.com/developers/applications (the image of name you put in)
+large_text : "USING CODE FNBRChris" // this is text that will show once you hover over the image on the rpc
 },
 buttons : [{label : "Affilate Referal" , url : "https://store.epicgames.com/fnbrchris/fortnite"},{label : "Twitter",url : "https://twitter.com/FNBRChris"}]
-}
+} //                         ^ text                     ^ url which will redirect                          ^ more text the url which will redirect
 })
 })
 client.on('ready', () => {
-    console.log(`${clientName} is Online!`)
-})
-client.login({ clientId : "814144442461323345" }).catch(console.error);
+    console.log(`${clientName} is Online!`) // ready event -> this will log to the console if it launchs all good
+})            // client id from https://discord.com/developers/applications
+client.login({ clientId : "814144442461323345" }).catch(console.error); // console logging errors which should happen but just incase
